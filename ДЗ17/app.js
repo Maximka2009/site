e63f316dcb4c4ce4e9f92bfb473583eb5cb22aab
Maximka2009 <75164903+Maximka2009@users.noreply.github.com>
2021-01-08 20:49:01 +0300

    Add files via upload

---
+++ ДЗ17/app.js
@@ -0,0 +1,35 @@
+let q_1 = prompt("Java Script - это язык программирования?\n a)Да\nb)Нет");
+if (q_1 == "a") {
+    alert("Good")
+}
+else {
+    alert("Bad")
+}
+let q_2 = prompt("Java Script использует верблюжий регистр\na)True\nb)False");
+if (q_2 == "a") {
+    alert("Good")
+}
+else {
+    alert("Bad")
+}
+let q_3 = prompt("Модальные окна - это...\na)окна взоимадействие с пользователем или опавещений о чём-либо\nb)Окна моделей;");
+if (q_3 == "a") {
+    alert("Good")
+}
+else {
+    alert("Bad")
+}
+let q_4 = prompt("Какая из переменных обьявлена правильно?\n a) let mylet = 1;\nb) let myLet = 1;\nc)var my_Var = 1;");
+if (q_4 == "b") {
+    alert("Good")
+}
+else {
+    alert("Bad")
+}
+let q_5 = prompt("Что отличает синтаксис Java Script от других?\na)Много лишнего\nb)Простота\nc)Верблюжий регистр\nd)Всё это");
+if (q_5 == "c") {
+    alert("Good")
+}
+else {
+    alert("Bad")
+}
